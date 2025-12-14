4911fa5d5c68d1e94562350a1c7c850a20fc59fc
Kishorchandra65 <kishorchandra65@gmail.com>
2025-12-14 03:35:48 -1200

    statics

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),
+    os.path.join(BASE_DIR, 'p/static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
